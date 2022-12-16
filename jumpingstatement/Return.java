cac386ecd9e9b05e2f2c2d643b0bc2ee1d526e6b
GOPI-Ops <119690161+GOPI-Ops@users.noreply.github.com>
2022-12-16 09:03:10 +0530

    Add files via upload

---
+++ jumpingstatement/Return.java
@@ -0,0 +1,18 @@
+package jumpingstatement;
+
+public class Return
+{
+	public int show(int c, int d)
+	{
+		return c+d;
+	}
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Return obj = new Return();
+		System.out.println(" ---"+obj.show(10,20));
+		
+	}
+
+}
